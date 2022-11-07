30e9ae6fd7224b6e092f8b747bfc1190b3ef7ae1
zenlucia <zenarts2020@gmail.com>
2022-11-07 09:29:37 +0000

    resorted folders

--- weekfour/imgChangeJS/script.js
+++
@@ -1,7 +0,0 @@
-function turnOn() {
-    document.getElementById("theImage").src = "/jspic_bulbon.gif";
-}
-
-function turnOff() {
-    document.getElementById("theImage").src = "/jspic_bulboff.gif";
-}
